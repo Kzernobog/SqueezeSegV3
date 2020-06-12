76f25294e6b91c357c681d3d8b590b88a807ff1e
Kzernobog <adityakamireddypalli@gmail.com>
2020-06-12 16:13:38 +0530

    Check if the inference code ran right
    Yes it did!!

---
+++ generate_seq8_pred.sh
@@ -0,0 +1,2 @@
+cd ./src/tasks/semantic/
+python infer.py -d /scratch/adityaRRC/dataset -l /scratch/adityaRRC/predictions -m /home/aditya/calibration_ws/SqueezeSegV3/models/SSGV3-21
---
+++ src/tasks/semantic/sample_delete.py
@@ -0,0 +1,5 @@
+def add_three_numbers(a, b, c):
+a = 4
+b = 5
+c = 4
+return a+b+c
